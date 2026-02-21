64f513502068dbcf95e310058a604756701f6399
Jyoti <jyotimiishra2003@gmail.com>
2026-02-21 15:47:16 +0530

    Connect Supabase to React and fetch crime data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useEffect } from 'react'
 import './App.css'
+import { supabase } from './Services/supabaseClient'
 
 function App() {
+
+  useEffect(() => {
+    const fetchData= async()=>{
+      const {data : crimes}= await supabase .from("crime_points") .select("*")
+      
+      const {data : lights}= await supabase .from("street_lights") .select("*")
+        console.log("Crime Data", crimes)
+        console.log("Street lights",lights)
+      
+    }
+    fetchData()
+   
+  }, [])
+  
+  
+
 
   return (
     <>
     <div className='flex items-center justify-center h-screen bg-black text-white'>
       <h3 className='font-bold text-4xl'>
-          Safe Route Ai
+          SupaBase connected!
       </h3>
     </div>
     </>
---
+++ src/Services/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  "https://clkllamdzngssvmzmtht.supabase.co",
+  "sb_publishable_iE7ItIPnGumn3lyV2ZPeVQ_B5iso2gX"
+)
